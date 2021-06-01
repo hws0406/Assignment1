f42bb9143ff53f5a46eacf1a81c32ac918c77107
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-06-02 06:03:47 +0800

    Create ClothingRentalService.java

---
+++ ClothingRentalService.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;	
+public class ClothingRentalService {
+	
+	Scanner input = new Scanner(System.in);
+	
+	String name, address, contact, email;
+
+	public ClothingRentalService(String name) {
+		
+		this.name = name;
+		this.address = "117, Pusat Perdagangan Pontian, 82000 Pontian Johor";
+		this.contact = "07-68704060";
+		this.email = "hws4600@hotmail.com";		
+	}
+	
+	public void printDecription() {
+		System.out.println("\n===================About Us===================");
+		System.out.printf("Company Name:", this.name);
+		System.out.printf("Company Address:", this.address);
+		System.out.printf("No H/P:", this.contact);
+		System.out.printf("Email:", this.email);
+	}
+}
